229ac4a06a635e580ceb3304eb445f5ec5d1fd11
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2024-06-22 01:48:41 -0500

    Commit

--- app/app.sh
+++ app/app.sh
@@ -2,4 +2,4 @@ sudo docker exec -it webappserver-node /bin/bash -c  "sudo aws dynamodb describe
 
 sudo docker exec -it webappserver-node /bin/bash -c  "sudo aws dynamodb list-tables --endpoint-url http://dynamodb-node:8000 --region us-east-1"
 
-sudo docker exec -it webappserver-node /bin/bash -c  "sudo aws dynamodb create-table --table-name samplecars --attribute-definitions AttributeName=manufacturer,AttributeType=S AttributeName=model,AttributeType=S --key-schema AttributeName=manufacturer,KeyType=HASH AttributeName=model,KeyType=RANGE --billing-mode PAY_PER_REQUEST --endpoint-url http://dynamodb-node:8000 --region us-east-1 --profile default"
+sudo docker exec -it webappserver-node /bin/bash -c  "sudo aws dynamodb create-table --table-name carsales-table --attribute-definitions AttributeName=manufacturer,AttributeType=S AttributeName=model,AttributeType=S --key-schema AttributeName=manufacturer,KeyType=HASH AttributeName=model,KeyType=RANGE --billing-mode PAY_PER_REQUEST --endpoint-url http://dynamodb-node:8000 --region us-east-1 --profile default"
